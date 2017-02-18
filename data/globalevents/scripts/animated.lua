6bfdc98bc31f2b3e3cc7d4f9e7b281736aeed8e1
Sekk <kicknclash@gmail.com>
2017-02-18 21:24:40 -0200

    .

--- data/globalevents/scripts/animated.lua
+++ data/globalevents/scripts/animated.lua
@@ -1,13 +1,14 @@
 local positions = {
     ["Heromassa"] = {x = 10157, y = 10053, z = 7},
-    ["Teleports"] = {x = 10143, y = 10042, z = 7},
+    ["Teleports"] = {x = 10155, y = 10042, z = 7},
 	["Vip City"] = {x = 10157, y = 10049, z = 7},
     ["Deposit"] = {x = 10169, y = 10055, z = 7},
-	["BattleEvt"] = {x = 10144, y = 10042, z = 7},
-	["Castle24H"] = {x = 10141, y = 10042, z = 7},
-	["Supreme"] = {x = 10142, y = 10042, z = 7},
-	["Trainers"] = {x = 10145, y = 10042, z = 7},
-	["Lord Vampire"] = {x = 10146, y = 10042, z = 7}
+	["Castle24H"] = {x = 10156, y = 10047, z = 7},
+	["Supreme"] = {x = 10158, y = 10047, z = 7},
+	["Trainers"] = {x = 10156, y = 10042, z = 7},
+	["CTF"] = {x = 10146, y = 10043, z = 7},
+	["BattleEvt"] = {x = 10142, y = 10043, z = 7},
+	["Zombie"] = {x = 10150, y = 10043, z = 7}
 }
 
 function onThink(cid, interval, lastExecution)
